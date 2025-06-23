493294ce1415864b1c3a8dc56d8359f0e6432f5b
Sahil <sahil@Sahil-MacBook.local>
2025-06-23 14:09:27 +0530

    chnages made to diffusion

--- misinformation_status.js
+++ misinformation_status.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const scoreBox = document.getElementById("score-box");
-    const finalScore = sessionStorage.getItem("misinformationScore") || "N/A";
-    
-    scoreBox.textContent = `Score: ${finalScore}%`;
+    const finalScore = sessionStorage.getItem("misinformationScore");
+
+    if (finalScore) {
+        scoreBox.textContent = `Score: ${finalScore}%`;
+    } else {
+        scoreBox.textContent = "Score not available";
+    }
 });
